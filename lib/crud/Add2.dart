2f3244010fbd290da083cb94c6146231dd8c7f6b
Zainab Benkrimi <zaina.benkrimi43@gmail.com>
2023-05-09 16:27:04 +0100

    sound1

--- lib/crud/Add2.dart
+++ lib/crud/Add2.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AddPhoto extends StatefulWidget {
   const AddPhoto({super.key});
