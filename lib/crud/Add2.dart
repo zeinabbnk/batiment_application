cd96eb605063dc14f5cc61daca503528caa9d6e4
Zainab Benkrimi <zaina.benkrimi43@gmail.com>
2023-05-12 20:36:10 +0100

    updates

--- lib/crud/Add2.dart
+++ lib/crud/Add2.dart
@@ -1,13 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 class AddPhoto extends StatefulWidget {
   const AddPhoto({super.key});
@@ -17,6 +15,7 @@ class AddPhoto extends StatefulWidget {
 }
 
 class _AddPhotoState extends State<AddPhoto> {
+  var SelectedPan = null;
 //fonction pour prendre les photos
   File? _photo;
 //take photo function
@@ -43,61 +42,6 @@ class _AddPhotoState extends State<AddPhoto> {
     final name = basename(imagePath);
     final image = File('${directory.path}/$name');
     return File(imagePath).copy(image.path);
-  }
-
-//fonctions pour enregister audio
-  @override
-  void initState() {
-    initRecord();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    recorder.closeRecorder();
-    super.dispose();
-  }
-
-//recording/playing audio Variable
-  final recorder = FlutterSoundRecorder();
-  bool isRecorderReady = false;
-
-  final audioPlayer = AudioPlayer();
-  bool isPlaying = false;
-  Duration durationS = Duration.zero;
-  Duration position = Duration.zero;
-
-//formatTime
-  String formatTime(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(duration.inHours);
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-
-    return [if (duration.inHours > 0) hours, minutes, seconds].join(':');
-  }
-
-  // microphone access
-  Future initRecord() async {
-    final status = await Permission.microphone.request();
-    if (status != PermissionStatus.granted) {
-      throw 'Permission not granted';
-    }
-    await recorder.openRecorder();
-
-    isRecorderReady = true;
-    recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
-  }
-
-  //2 functions for starting record an stop
-  Future starRecorder() async {
-    await recorder.startRecorder(toFile: 'audio');
-  }
-
-  Future stopRecord() async {
-    final filePath = await recorder.stopRecorder();
-    final file = File(filePath!);
-    print('Recorder file path is : $file');
   }
 
   @override
@@ -179,132 +123,57 @@ class _AddPhotoState extends State<AddPhoto> {
           SizedBox(
             height: 30,
           ),
-          //Recording Audio
-          //recording timer
-          StreamBuilder<RecordingDisposition>(
-            builder: (context, snapshot) {
-              final duration =
-                  snapshot.hasData ? snapshot.data!.duration : Duration.zero;
 
-              String twoDigits(int n) => n.toString().padLeft(2, '0');
-              final twoDigitsMinites =
-                  twoDigits(duration.inMinutes.remainder(60));
-              final twoDigitsSeconds =
-                  twoDigits(duration.inSeconds.remainder(60));
-
-              return Text(
-                "$twoDigitsMinites : $twoDigitsSeconds",
-                style: TextStyle(
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: DropdownSearch<String>(
+              popupProps: PopupProps.menu(
+                showSelectedItems: true,
+                disabledItemFn: (String s) => s.startsWith('I'),
+              ),
+              items: [
+                "Pannes électriques",
+                "Pannes de plomberie",
+                "Pannes de toiture ",
+                'Pannes de serrurerie',
+                "Problème de peinture",
+                "Problèmes d'humidité",
+                "Autres"
+              ],
+              dropdownDecoratorProps: DropDownDecoratorProps(
+                dropdownSearchDecoration: InputDecoration(
+                  icon: Icon(
+                    Icons.house,
                     color: Color(0xFF2B3467),
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-              );
-            },
-            //updating the recording
-            stream: recorder.onProgress,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              Container(
-                width: 180,
-                height: 85,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (recorder.isRecording) {
-                      await stopRecord();
-                      setState(() {});
-                    } else {
-                      await starRecorder();
-                      setState(() {});
-                    }
-                  },
-                  child:
-                      //record Button
-                      Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                          recorder.isRecording
-                              ? Icons.stop
-                              : Icons.mic_outlined,
-                          size: 28,
-                          color: Color(0xFF2B3467)),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        recorder.isRecording ? "Stop" : "Record",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF2B3467)),
-                      )
-                    ],
                   ),
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFDBDFEA),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
+                  labelText: "Type de Panne",
+                  labelStyle: TextStyle(
+                      color: Color(0xFF2B3467), fontWeight: FontWeight.bold),
+                  hintText: "Choisir le Type de panne",
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide:
+                          BorderSide(color: Color(0xFFBAD7E9), width: 3)),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Color(0xFFBAD7E9), width: 3),
+                  ),
                 ),
               ),
-              //playing Audio Button
-              Container(
-                width: 180,
-                height: 85,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    //  if (isPlaying) {
-                    //    await audioPlayer.pause();
-                    //  } else {
-                    //
-                    //  }
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.play_arrow,
-                          size: 28, color: Color(0xFF2B3467)),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Play",
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF2B3467)),
-                      )
-                    ],
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFDBDFEA),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                ),
-              )
-            ],
-          ),
-          Slider(
-            min: 0,
-            max: durationS.inSeconds.toDouble(),
-            value: position.inSeconds.toDouble(),
-            onChanged: (value) async {},
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(formatTime(position)),
-                Text(formatTime(durationS - position)),
-              ],
+              onChanged: (val) {
+                setState(() {
+                  SelectedPan = val;
+                });
+              },
+              selectedItem: SelectedPan,
             ),
-          )
+          ),
+
+          SizedBox(
+            height: 30,
+          ),
+          //Recording Audio
+          //recording timer
         ]),
       ),
     );
