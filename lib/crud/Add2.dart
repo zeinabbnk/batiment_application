970c766a07117e3dca5f94fedd73cf93aee4a3f7
Zainab Benkrimi <zaina.benkrimi43@gmail.com>
2023-05-15 15:04:49 +0100

    modif6

--- lib/crud/Add2.dart
+++ lib/crud/Add2.dart
@@ -6,6 +6,9 @@ import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class AddPhoto extends StatefulWidget {
   const AddPhoto({super.key});
@@ -42,6 +45,60 @@ class _AddPhotoState extends State<AddPhoto> {
     final name = basename(imagePath);
     final image = File('${directory.path}/$name');
     return File(imagePath).copy(image.path);
+  }
+
+//fonctions pour enregister audio
+  @override
+  void initState() {
+    initRecord();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    recorder.closeRecorder();
+    super.dispose();
+  }
+
+//recording/playing audio Variable
+  final recorder = FlutterSoundRecorder();
+  bool isRecorderReady = false;
+
+  bool isPlaying = false;
+  Duration durationS = Duration.zero;
+  Duration position = Duration.zero;
+
+//formatTime
+  String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return [if (duration.inHours > 0) hours, minutes, seconds].join(':');
+  }
+
+  // microphone access
+  Future initRecord() async {
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw 'Permission not granted';
+    }
+    await recorder.openRecorder();
+
+    isRecorderReady = true;
+    recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
+  }
+
+  //2 functions for starting record an stop
+  Future starRecorder() async {
+    await recorder.startRecorder(toFile: 'audio');
+  }
+
+  Future stopRecord() async {
+    final filePath = await recorder.stopRecorder();
+    final file = File(filePath!);
+    print('Recorder file path is : $file');
   }
 
   @override
@@ -137,8 +194,6 @@ class _AddPhotoState extends State<AddPhoto> {
                 "Pannes de toiture ",
                 'Pannes de serrurerie',
                 "Problème de peinture",
-                "Problèmes d'humidité",
-                "Autres"
               ],
               dropdownDecoratorProps: DropDownDecoratorProps(
                 dropdownSearchDecoration: InputDecoration(
@@ -174,6 +229,70 @@ class _AddPhotoState extends State<AddPhoto> {
           ),
           //Recording Audio
           //recording timer
+          StreamBuilder<RecordingDisposition>(
+            builder: (context, snapshot) {
+              final duration =
+                  snapshot.hasData ? snapshot.data!.duration : Duration.zero;
+
+              String twoDigits(int n) => n.toString().padLeft(2, '0');
+              final twoDigitsMinites =
+                  twoDigits(duration.inMinutes.remainder(60));
+              final twoDigitsSeconds =
+                  twoDigits(duration.inSeconds.remainder(60));
+
+              return Text(
+                "$twoDigitsMinites : $twoDigitsSeconds",
+                style: TextStyle(
+                    color: Color(0xFF2B3467),
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              );
+            },
+            //updating the recording
+            stream: recorder.onProgress,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 180,
+            height: 85,
+            padding: EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: () async {
+                if (recorder.isRecording) {
+                  await stopRecord();
+                  setState(() {});
+                } else {
+                  await starRecorder();
+                  setState(() {});
+                }
+              },
+              child:
+                  //record Button
+                  Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(recorder.isRecording ? Icons.stop : Icons.mic_outlined,
+                      size: 28, color: Color(0xFF2B3467)),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    recorder.isRecording ? "Stop" : "Record",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF2B3467)),
+                  )
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Color(0xFFDBDFEA),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+          ),
         ]),
       ),
     );
